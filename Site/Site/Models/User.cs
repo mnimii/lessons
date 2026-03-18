067f30a159fb459b1fdfe9f11a1701fa237ba231
user <user@GAMEPC>
2026-03-18 22:44:46 +0200

    Добавьте файлы проекта.

---
+++ Site/Site/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Site.Models
+{
+    public class User
+    {
+        public int Id { get; set; } = 0;
+        public string Login { get; set; } = "";
+        public string Password { get; set; } = "";
+    }
+}
---
+++ Site/Site/Serveces/UserServis.cs
@@ -0,0 +1,67 @@
+﻿using Site.Models;
+
+namespace Site.Serveces
+{
+    public class UserServis
+    {
+        private List<User> _users;
+        private int _lastId = 0;
+
+        public UserServis()
+        {
+            _users = new();
+            Startup();
+        }
+
+        public void GetDataFromSource()
+        {
+            _users.Add(new User() { Id = GetCurrentNextId(), Login = "log1", Password = "1111" });
+            _users.Add(new User() { Id = GetCurrentNextId(), Login = "log2", Password = "2222" });
+            _users.Add(new User() { Id = GetCurrentNextId(), Login = "log3", Password = "3333" });
+        }
+
+        public void Startup()
+        {
+            GetDataFromSource();
+        }
+
+        private int GetCurrentNextId() => _lastId++;
+
+        public List<User> GetAll() => _users;
+
+        public User GetById(int id) => _users.FirstOrDefault(x => x.Id == id);
+
+        public User Add(User user)
+        {
+            user.Id = GetCurrentNextId();
+            _users.Add(user);
+
+            return user;
+        }
+
+        public bool Update(User user)
+        {
+            var found_user = GetById(user.Id);
+
+            if (found_user == null)
+                return false;
+
+            found_user.Login = user.Login;
+            found_user.Password = user.Password;
+
+            return true;
+        }
+
+        public bool DeleteBuId(int id)
+        {
+            var found_user = GetById(id);
+            
+            if (found_user == null)
+                return false;
+
+            _users.Remove(found_user);
+            return true;
+
+        }
+    }
+}
